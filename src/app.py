65123b07a3afc580cf98a4a3b4cece279e61c7ff
Kaleb Williams <kalebklw@gmail.com>
2025-09-18 01:14:48 +0000

    Created functionality

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Event
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,63 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    users = User.query.all()
+    users_list = [item.serialize() for item in users]
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ",
+        "data": users_list
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/event', methods=['GET'])
+def event_hello():
+    event = Event.query.all()
+    event_list = [item.serialize() for item in event]
+
+    return jsonify(event_list), 200
+
+
+@app.route('/event', methods=['POST'])
+def event_add():
+    body = request.get_json()
+    name = body['name']
+    event_planner = body['event_planner']
+    new_event = Event(
+        name=name,
+        event_planner=event_planner
+    )
+    db.session.add(new_event)
+    db.session.commit()
+
+    return jsonify(new_event.serialize())
+
+
+@app.route('/event/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event =  db.session.get(Event, event_id)
+    db.session.add(event)
+    db.session.commit()
+
+    return jsonify(event.serialize()), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
